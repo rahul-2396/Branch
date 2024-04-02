e4a7c7e70dacdf1525b58a5ef4fc2ac5398f1edc
Rahul <023gameralcatraz@gmail.com>
2024-04-02 20:58:26 +0530

    Added from Intellij!

---
+++ AdditionCalculator.java
@@ -0,0 +1,7 @@
+public class AdditionCalculator
+{
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        System.out.println("Addition is " + AdditionCalculator.add(10, 20));
+    }
+}
